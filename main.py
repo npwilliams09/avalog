e86a1a5c847053d48fbe441d8def7477b79ffd29
Nathan Williams <npwilliams09@gmail.com>
2020-04-28 17:31:13 +1000

    updated functionality

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 
 # [START gae_python37_app]
 from flask import Flask
-
+from flask import request
+from google.cloud import firestore
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
+@app.route('/',methods = ['GET', 'POST'])
+def process():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    db = firestore.Client() #create db client
+
+    if (request.method == 'GET'):
+        #get from firestore with sql query
+        return str(db.collection(u'avalanches').where(u'size', u'==', u'1'))
+
+    if (request.method == 'POST'):
+        #store data in firestore
+        data = request.form
+        db.collection(u'avalanches').document(u'dummy').set(data)
+        return 'data posted'
+
+
 
 
 if __name__ == '__main__':
